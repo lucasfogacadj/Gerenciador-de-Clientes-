aca65ae12d679db5fb1c34781449a8d504795141
Lucas Fogaça <lucasfgc00@gmail.com>
2018-04-04 15:26:50 -0300

    first
    comitt

--- model/DAO/BaseDAO.php
+++ model/DAO/BaseDAO.php
@@ -28,7 +28,7 @@ abstract class BaseDAO
             $parametros    = $cols;
             $colunas       = str_replace(":", "", $cols);
             /*
-                INSERT INTO usuario (nome,email) VALUES (:nome,:email);
+                INSERT INTO Clientes (nome,telefone) VALUES (:nome,:telefone);
             */
             $stmt = $this->conexao->prepare("INSERT INTO $table ($colunas) VALUES ($parametros)");
             $stmt->execute($values);
